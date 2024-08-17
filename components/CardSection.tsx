943738666dc72bf1a044b4cc3f8d3386a7c878fd
Adarsh Singh <adarsh9540984202@gmail.com>
2024-08-17 16:08:47 +0530

    make equall ticklist

--- components/CardSection.tsx
+++ components/CardSection.tsx
@@ -13,7 +13,7 @@ const CardSection: React.FC = () => {
   const features2 = [
     'Replace unreliable and overpriced LinkedIn automation tools.',
     '8+ LinkedIn searches, integrations with Clay,Smartlead, and Instantly.',
-    'Get Campaign results, A/B tests, each sender performance.',
+    'Get detailed Campaign results, A/B tests, each sender performance.',
     'Add tags, filters and have all your conversations in one simple inbox.',
     'Export LinkedIn contacts from search, Sales Nav, groups,and more.',
   ]
